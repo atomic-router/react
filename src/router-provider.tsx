39761f47bf79c6b5bcac15c01ca57aed4417d6e3
Sergey Sova <mail@sergeysova.com>
2022-09-19 14:10:14 +0400

    ci: add linter

--- src/router-provider.tsx
+++ src/router-provider.tsx
@@ -1,20 +1,12 @@
-import { createHistoryRouter } from 'atomic-router';
-import React, { createContext, ReactNode, useContext } from 'react';
+import { createHistoryRouter } from "atomic-router";
+import React, { createContext, ReactNode, useContext } from "react";
 
 type Router = ReturnType<typeof createHistoryRouter>;
 
 export const RouterContext = createContext<Router | null>(null);
 
-export function RouterProvider({
-  router,
-  children,
-}: {
-  router: Router;
-  children: ReactNode;
-}) {
-  return (
-    <RouterContext.Provider value={router}>{children}</RouterContext.Provider>
-  );
+export function RouterProvider({ router, children }: { router: Router; children: ReactNode }) {
+  return <RouterContext.Provider value={router}>{children}</RouterContext.Provider>;
 }
 
 export function useRouter() {
