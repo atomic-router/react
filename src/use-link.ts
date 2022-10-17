a755a42d2bfa8a00c804f87d19fc5899b8143cda
Anton Kosykh <kelin2025@yandex.ru>
2022-10-17 18:38:09 +0300

    link: Re-format

--- src/use-link.ts
+++ src/use-link.ts
@@ -4,18 +4,18 @@ import { useRouter } from "./router-provider";
 export function useLink<T extends RouteParams>(
   route: RouteInstance<T>,
   params: T,
-  query: RouteQuery = {},
+  query: RouteQuery = {}
 ): string {
-  const router = useRouter()
+  const router = useRouter();
   const routeObj = router.routes.find((routeObj) => routeObj.route === route);
 
   if (!routeObj) {
-    throw new Error(`[useLink] Route not found. Maybe it is not passed into createHistoryRouter`)
+    throw new Error(`[useLink] Route not found. Maybe it is not passed into createHistoryRouter`);
   }
 
   return buildPath({
     pathCreator: routeObj.path,
     params: params,
     query: query,
-  })
+  });
 }
