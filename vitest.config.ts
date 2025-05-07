b64363544829f81c72c1d72d1e1e2975baaf9256
Sergey Sova <mail@sergeysova.com>
2025-05-07 19:52:02 +0400

    feat: allow atomic-router 0.11, upgrade deps, remove jest

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config";
+import babel from "@rollup/plugin-babel";
+
+const extensions = [".ts", ".tsx", ".js"];
+const babelPlugin = babel({
+  babelHelpers: "bundled",
+  sourceMaps: true,
+  extensions,
+  exclude: /node_modules.*/,
+});
+
+export default defineConfig({
+  plugins: [babelPlugin],
+});
